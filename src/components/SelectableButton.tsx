24ae78a6cdc6c8dbb65fbdd748b4c6d9d3528a1d
Joao Vitor Fernandes Vitor <joaovitorfernandesvitor@gmail.com>
2026-09-16 13:05:38 -0300

    Feat: Add Novo componente - SelectableButton.tsx; Ajustes no maxLength dos TextInputs e styles ViewSubjectRegister.tsx.

---
+++ src/components/SelectableButton.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { View, TouchableOpacity, Text,StyleSheet, PressableProps} from 'react-native';
+import { colors, typography } from '@/theme';
+
+/**
+ * PressableButton é um botão que alterna sua aparencia quando pressionado.
+ * @param text - O texto do botão
+ */
+
+type SelectableButtonProps = PressableProps & {
+    text: string;
+};
+
+export function SelectableButton({text, onPress}: SelectableButtonProps){
+
+  const [select, setSelect] = useState(false);
+
+  const handlePress = () => {
+    setSelect(!select); //Altera entre selecionado/não selecionado
+
+    if(onPress)
+      onPress?.();  // chama a função vinda do pai (com segurança, caso não exista)
+  };
+
+  return(
+    <View>
+      {select ? //Baseado na select altena a aparencia do botão entre os dois modelos
+        //Versão Não Selecionado
+        <TouchableOpacity style={styles.buttonSelect} onPress={handlePress}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </TouchableOpacity>
+        :
+        //Versão Selecionado
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </TouchableOpacity>
+      }
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    buttonText: {
+        fontFamily: typography.fontFamily.extrabold,
+        fontSize: typography.fontSize['sm'],
+        color: colors.text,
+    },
+    button: {
+    backgroundColor: colors.primaryBlue,
+    padding: 14,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 8,
+    },
+    buttonSelect: {
+    backgroundColor: colors.attention,
+    padding: 14,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 8,
+    }
+});
--- src/components/ViewSubjectRegister.tsx
+++ src/components/ViewSubjectRegister.tsx
@@ -1,7 +1,8 @@
 import {useState} from 'react';
-import { View, Text, TextInput, StyleSheet} from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView} from 'react-native';
 import { colors, typography } from '@/theme';
 
+import {SelectableButton} from '@/components/SelectableButton';
 
 export function ViewSubjectRegister () {
     const [subjectName, onChangeSubjectName] = useState('');
@@ -9,46 +10,62 @@ export function ViewSubjectRegister () {
     const [block, onChangeBlock] = useState('');
     const [professorName, onChangeProfessorName] = useState('');
 
+    const [visibleSeg, setVisibleSeg] = useState(false);
+
     return(
         <View>
+            <View>
+                <Text style = { styles.title}> Registro de Materia </Text>
 
-            <Text style = { styles.title}> Registro de Materia </Text>
-
-            //TextInput dos campos do registro
-            <TextInput
-                style ={styles.input}
-                placeholder='Nome da Materia'
-                onChangeText = {onChangeSubjectName}
-                value = {subjectName}
-                keyboardType = 'default'
-                returnKeyType = 'next'
-            />
-            <View style = { styles.rowContainer }>
+                //TextInput dos campos do registro
                 <TextInput
                     style ={styles.input}
-                    placeholder='Sala'
-                    onChangeText = {onChangeClassNumber}
-                    value = {classNumber}
-                    keyboardType = 'numeric'
+                    placeholder='Nome da Materia'
+                    onChangeText = {onChangeSubjectName}
+                    value = {subjectName}
+                    maxLength = {50}
+                    keyboardType = 'default'
                     returnKeyType = 'next'
                 />
+                <View style = { styles.rowContainer }>
+                    <TextInput
+                        style ={styles.input}
+                        placeholder='Sala'
+                        onChangeText = {onChangeClassNumber}
+                        value = {classNumber}
+                        maxLength = {2}
+                        keyboardType = 'numeric'
+                        returnKeyType = 'next'
+                    />
+                    <TextInput
+                        style ={styles.input}
+                        placeholder='Bloco'
+                        onChangeText = {onChangeBlock}
+                        value = {block}
+                        maxLength = {1}
+                        keyboardType = 'default'
+                        returnKeyType = 'next'
+                    />
+                </View>
                 <TextInput
                     style ={styles.input}
-                    placeholder='Bloco'
-                    onChangeText = {onChangeBlock}
-                    value = {block}
+                    placeholder='Nome do Professor'
+                    onChangeText = {onChangeProfessorName}
+                    value = {professorName}
+                    maxLength = {50}
                     keyboardType = 'default'
                     returnKeyType = 'next'
                 />
             </View>
-            <TextInput
-                style ={styles.input}
-                placeholder='Nome do Professor'
-                onChangeText = {onChangeProfessorName}
-                value = {professorName}
-                keyboardType = 'default'
-                returnKeyType = 'next'
-            />
+            <Text style = { styles.title}> Horários </Text>
+            <View style = {styles.rowContainer}>
+                <SelectableButton text = 'Seg' />
+                <SelectableButton text = 'Ter' />
+                <SelectableButton text = 'Qua' />
+                <SelectableButton text = 'Qui' />
+                <SelectableButton text = 'Sex' />
+                <SelectableButton text = 'Sab' />
+            </View>
         </View>
     )
 }
@@ -73,16 +90,15 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: colors.white,
         padding: 16,
-        minWidth: 100,
         borderRadius: 12,
         marginBottom: 8,
         boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.05)',
         fontFamily: typography.fontFamily.regular
     },
-
     rowContainer: {
         flexDirection: 'row',       // alinha os filhos na horizontal
         alignItems: 'center',       // centraliza verticalmente
         justifyContent: 'space-between', // distribui o espaço entre eles
-        }
+        gap: 5
+    }
 });
